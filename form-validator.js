9028e1b75b8b1eb0236a31f8354954530a6c99e2
cokeefe <chaemobile@gmail.com>
2013-08-15 11:27:20 -0700

    first commit:

--- form-validator.js
+++ form-validator.js
@@ -1,5 +1,16 @@
 // shorthand for $(document).ready();
 $(function(){
-  //Your code...
-
+  // $('#errors').append("<li></li>")
 });
+
+
+// When the user clicks the "Sign Up" button
+// They should be notified if any of the following conditions are NOT true
+// - The email conforms to the standard pattern
+// - The password has at least 8 characters
+// - The password has at least one capital letter
+// - The password has at least one numeric character
+
+// If any of the above conditions are false
+// - The form is not allowed to be submitted
+// - Error messages are dislpayed
