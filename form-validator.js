353d7876c055ca698a968207d2ef15cb095b3a5a
cokeefe <chaemobile@gmail.com>
2013-08-15 15:22:51 -0700

    add validate prototype method

--- form-validator.js
+++ form-validator.js
@@ -1,9 +1,51 @@
 // shorthand for $(document).ready();
+function Validator(){
+  this.errors = [];
+  this.valid = true
+};
+
+Validator.prototype.checkEmail = function(email){
+  //come back for regex
+  //a = regex
+  if (email != "a"){
+    this.errors.push("Must be a valid email");
+    this.valid = false;
+  };
+};
+
+Validator.prototype.validate = function(email, password){
+  this.checkEmail(email);
+};
+
 $(function(){
-  // $('#errors').append("<li></li>")
+
+  validator = new Validator();
+
+  $('form').on('submit', function(e){
+    e.preventDefault();
+    $('#errors').empty();
+
+    var email = $(':input[type="text"]').val();
+    var pswd = $(':input[type="password"]').val();
+
+    validator.validate(email)
+
+    if (validator.valid == false) {
+      console.log(validator.errors);
+
+      $.each(validator.errors, function(index, value){
+        $('#errors').append("<li>" + value + "</li>")
+      });
+
+
+      validator.valid = true;
+      validator.errors = [];
+    }
+
+  });
 });
 
-
+    // $('#errors').append("<li></li>")
 // When the user clicks the "Sign Up" button
 // They should be notified if any of the following conditions are NOT true
 // - The email conforms to the standard pattern
